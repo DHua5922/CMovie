f2b77564c7d8cfa0737475b178f3b29acc361d0b
DHua5922 <hua.dylan@gmail.com>
2021-04-16 07:18:17 -0500

    CU-rq76dp - Create Navbar

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './tailwind.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './pages/App/App';
 import reportWebVitals from './reportWebVitals';
 import "./api/request";
